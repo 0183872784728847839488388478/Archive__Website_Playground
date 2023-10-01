8a571623db0a24afbad26a4addafafcd191f22f2
corrupteddroid <corrupteddroid@gmail.com>
2023-10-01 14:03:29 +0800

    00

--- numbers/js/index.js
+++ numbers/js/index.js
@@ -16,17 +16,17 @@ function display_set(nyan,number) {
 };
 
 function display_hide(nyan) {
-    document.getElementById("slot_"+nyan).setAttribute("value");
+    document.getElementById("slot_"+nyan).setAttribute("value",10);
 };
 
 function display_delete(nyan) {
-    document.getElementById("slot_"+nyan).setAttribute("value");
+    document.getElementById("slot_"+nyan).setAttribute("value",10);
     numberlist[nyan] = 10;
 };
 
 
 function game_shuffle() {
-    for (var i = 0; i < 9; i++) {
+    for (var i = 0; i < 10; i++) {
         let number = Math.floor((Math.random() * 10));
         display_create(i,number)
         display_hide(i)
@@ -42,12 +42,12 @@ function game_hide(number){
 }
 
 function game_show_all(){
-    for (var i = 0; i < 9; i++) {
+    for (var i = 0; i < 10; i++) {
         display_set(i,numberlist[i])
     }
 }
 function game_hide_all(){
-    for (var i = 0; i < 9; i++) {
+    for (var i = 0; i < 10; i++) {
         display_hide(i)
     }
 }
@@ -59,30 +59,38 @@ function sleep(ms) {
 
 
 function startTimer(duration) {
-    display = document.querySelector('#time');
+    if (timer = timer) {
+        timer = null
+    }
+    clearInterval(endinterval)
     var timer = duration, minutes, seconds;
     document.getElementById('progress_time').setAttribute("max", duration);
-    setInterval(function () {
-        minutes = parseInt(timer / 60, 10);
-        seconds = parseInt(timer % 60, 10);
+    var endinterval = setInterval(
+            function () {
+                minutes = parseInt(timer / 60, 10);
+                seconds = parseInt(timer % 60, 10);
 
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
+                minutes = minutes < 10 ? "0" + minutes : minutes;
+                seconds = seconds < 10 ? "0" + seconds : seconds;
 
-        display.textContent = minutes + ":" + seconds;
+                game_show_random()
+                if (--timer < 0) {
+                    document.getElementById('time_seconds_tens').setAttribute("value",10);
+                    document.getElementById('time_seconds_ones').setAttribute("value",10);
+                    game_hide_all()
+                    clearInterval(endinterval)
+                }
 
-        if (--timer < 0) {
-            timer = 0;
-        }
-        tpoqoieow = minutes + "" + seconds;
-        uiotret = tpoqoieow.split("");
-        document.getElementById('progress_time').setAttribute("value", seconds);
-        document.getElementById('time_seconds_tens').setAttribute("value", uiotret[2]);
-        document.getElementById('time_seconds_ones').setAttribute("value", uiotret[3]);
-    }, 1000);
+                tpoqoieow = minutes + "" + seconds;
+                uiotret = tpoqoieow.split("");
+                document.getElementById('progress_time').setAttribute("value", seconds);
+                document.getElementById('time_seconds_tens').setAttribute("value", uiotret[2]);
+                document.getElementById('time_seconds_ones').setAttribute("value", uiotret[3]);
+        }, 1000);
 }
 
-
-    game_show_all()
-    game_hide_all();
-    startTimer(30);
+function game_show_random() {
+    game_hide_all()
+    let gshdkldfs = Math.floor((Math.random() * 10));
+    display_set(gshdkldfs,numberlist[gshdkldfs])
+}
