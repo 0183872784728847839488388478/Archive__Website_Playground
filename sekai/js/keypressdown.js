2e870cdecc53d9ae64706d674f99fd1eb13352bd
corrupteddroid <corrupteddroid@gmail.com>
2023-10-07 15:28:19 +0800

    0

--- sekai/js/keypressdown.js
+++ sekai/js/keypressdown.js
@@ -1,6 +1,6 @@
 // Add event listener on keypress
 var color_unpressed = "dimgray"
-var color_pressed = "#00CDBA"
+var color_pressed = "blue"
 var color_instants = "red"
 document.addEventListener('keypress', (event) => {
 	var name = event.key;
@@ -12,52 +12,52 @@ document.addEventListener('keypress', (event) => {
 	if (name == "q") {
 		document.getElementById("q").style.background = color_pressed;
 		console.log("Match Found: "+ name);
-		audio0_0.play();
+		playsound("none")
 	};
 	if (name == "w") {
 		document.getElementById("w").style.background = color_pressed;
 		console.log("Match Found: "+ name);
-		audio1_0.play();
+		playsound("tap")
 	};
 	if (name == "e") {
 		document.getElementById("e").style.background = color_pressed;
 		console.log("Match Found: "+ name);
-		audio2_0.play();
+		playsound("tap")
 	};
 	if (name == "r") {
 		document.getElementById("r").style.background = color_pressed;
 		console.log("Match Found: "+ name);
-		audio3_0.play();
+		playsound("tap")
 	};
 	if (name == "t") {
 		document.getElementById("t").style.background = color_pressed;
 		console.log("Match Found: "+ name);
-		audio4_0.play();
+		playsound("tap")
 	};
 	if (name == "y") {
 		document.getElementById("y").style.background = color_pressed;
 		console.log("Match Found: "+ name);
-		audio5_0.play();
+		playsound("tap")
 	};
 	if (name == "u") {
 		document.getElementById("u").style.background = color_pressed;
 		console.log("Match Found: "+ name);
-		audio6_0.play();
+		playsound("tap")
 	};
 	if (name == "i") {
 		document.getElementById("i").style.background = color_pressed;
 		console.log("Match Found: "+ name);
-		audio7_0.play();
+		playsound("tap")
 	};
 	if (name == "o") {
 		document.getElementById("o").style.background = color_pressed;
 		console.log("Match Found: "+ name);
-		audio8_0.play();
+		playsound("tap")
 	};
 	if (name == "p") {
 		document.getElementById("p").style.background = color_pressed;
 		console.log("Match Found: "+ name);
-		audio9_0.play();
+		playsound("tap")
 	};
 }, false);
 
@@ -71,52 +71,52 @@ document.addEventListener('keydown', (event) => {
 	if (name == "q") {
 		document.getElementById("q").style.background = color_pressed;
 		console.log("Match Found: "+ name);
-		audio0_1.play();
+		
 	};
 	if (name == "w") {
 		document.getElementById("w").style.background = color_pressed;
 		console.log("Match Found: "+ name);
-		audio1_1.play();
+		
 	};
 	if (name == "e") {
 		document.getElementById("e").style.background = color_pressed;
 		console.log("Match Found: "+ name);
-		audio2_1.play();
+		
 	};
 	if (name == "r") {
 		document.getElementById("r").style.background = color_pressed;
 		console.log("Match Found: "+ name);
-		audio3_1.play();
+		
 	};
 	if (name == "t") {
 		document.getElementById("t").style.background = color_pressed;
 		console.log("Match Found: "+ name);
-		audio4_1.play();
+		
 	};
 	if (name == "y") {
 		document.getElementById("y").style.background = color_pressed;
 		console.log("Match Found: "+ name);
-		audio5_1.play();
+		
 	};
 	if (name == "u") {
 		document.getElementById("u").style.background = color_pressed;
 		console.log("Match Found: "+ name);
-		audio6_1.play();
+		
 	};
 	if (name == "i") {
 		document.getElementById("i").style.background = color_pressed;
 		console.log("Match Found: "+ name);
-		audio7_1.play();
+		
 	};
 	if (name == "o") {
 		document.getElementById("o").style.background = color_pressed;
 		console.log("Match Found: "+ name);
-		audio8_1.play();
+		
 	};
 	if (name == "p") {
 		document.getElementById("p").style.background = color_pressed;
 		console.log("Match Found: "+ name);
-		audio9_1.play();
+		
 	};
   }, false);
 
@@ -127,85 +127,59 @@ document.addEventListener('keyup', (event) => {
 	if (name == "q") {
 		document.getElementById("q").style.background = color_pressed;
 		console.log("Match Found: "+ name);
-		audio0_2.play();
+		
 	};
 	if (name == "w") {
 		document.getElementById("w").style.background = color_pressed;
 		console.log("Match Found: "+ name);
-		audio1_2.play();
+		
 	};
 	if (name == "e") {
 		document.getElementById("e").style.background = color_pressed;
 		console.log("Match Found: "+ name);
-		audio2_2.play();
+		
 	};
 	if (name == "r") {
 		document.getElementById("r").style.background = color_pressed;
 		console.log("Match Found: "+ name);
-		audio3_2.play();
+		
 	};
 	if (name == "t") {
 		document.getElementById("t").style.background = color_pressed;
 		console.log("Match Found: "+ name);
-		audio4_2.play();
+		
 	};
 	if (name == "y") {
 		document.getElementById("y").style.background = color_pressed;
 		console.log("Match Found: "+ name);
-		audio5_2.play();
+		
 	};
 	if (name == "u") {
 		document.getElementById("u").style.background = color_pressed;
 		console.log("Match Found: "+ name);
-		audio6_2.play();
+		
 	};
 	if (name == "i") {
 		document.getElementById("i").style.background = color_pressed;
 		console.log("Match Found: "+ name);
-		audio7_2.play();
+		
 	};
 	if (name == "o") {
 		document.getElementById("o").style.background = color_pressed;
 		console.log("Match Found: "+ name);
-		audio8_2.play();
+		
 	};
 	if (name == "p") {
 		document.getElementById("p").style.background = color_pressed;
 		console.log("Match Found: "+ name);
-		audio9_2.play();
+		
 	};
 
   }, false);
 
-var audio0_0 = new Audio('https://storage.sekai.best/sekai-assets/live/tap_se/default_rip/se_live_tap.mp3');
-var audio1_0 = new Audio('https://storage.sekai.best/sekai-assets/live/tap_se/default_rip/se_live_tap.mp3');
-var audio2_0 = new Audio('https://storage.sekai.best/sekai-assets/live/tap_se/default_rip/se_live_tap.mp3');
-var audio3_0 = new Audio('https://storage.sekai.best/sekai-assets/live/tap_se/default_rip/se_live_tap.mp3');
-var audio4_0 = new Audio('https://storage.sekai.best/sekai-assets/live/tap_se/default_rip/se_live_tap.mp3');
-var audio5_0 = new Audio('https://storage.sekai.best/sekai-assets/live/tap_se/default_rip/se_live_tap.mp3');
-var audio6_0 = new Audio('https://storage.sekai.best/sekai-assets/live/tap_se/default_rip/se_live_tap.mp3');
-var audio7_0 = new Audio('https://storage.sekai.best/sekai-assets/live/tap_se/default_rip/se_live_tap.mp3');
-var audio8_0 = new Audio('https://storage.sekai.best/sekai-assets/live/tap_se/default_rip/se_live_tap.mp3');
-var audio9_0 = new Audio('https://storage.sekai.best/sekai-assets/live/tap_se/default_rip/se_live_tap.mp3');
-
-//var audio0_1 = new Audio('https://storage.sekai.best/sekai-assets/live/tap_se/default_rip/se_live_long.mp3');
-//var audio1_1 = new Audio('https://storage.sekai.best/sekai-assets/live/tap_se/default_rip/se_live_long.mp3');
-//var audio2_1 = new Audio('https://storage.sekai.best/sekai-assets/live/tap_se/default_rip/se_live_long.mp3');
-//var audio3_1 = new Audio('https://storage.sekai.best/sekai-assets/live/tap_se/default_rip/se_live_long.mp3');
-//var audio4_1 = new Audio('https://storage.sekai.best/sekai-assets/live/tap_se/default_rip/se_live_long.mp3');
-//var audio5_1 = new Audio('https://storage.sekai.best/sekai-assets/live/tap_se/default_rip/se_live_long.mp3');
-//var audio6_1 = new Audio('https://storage.sekai.best/sekai-assets/live/tap_se/default_rip/se_live_long.mp3');
-//var audio7_1 = new Audio('https://storage.sekai.best/sekai-assets/live/tap_se/default_rip/se_live_long.mp3');
-//var audio8_1 = new Audio('https://storage.sekai.best/sekai-assets/live/tap_se/default_rip/se_live_long.mp3');
-//var audio9_1 = new Audio('https://storage.sekai.best/sekai-assets/live/tap_se/default_rip/se_live_long.mp3');
-
-//var audio0_2 = new Audio('https://storage.sekai.best/sekai-assets/live/tap_se/default_rip/se_live_flick.mp3');
-//var audio1_2 = new Audio('https://storage.sekai.best/sekai-assets/live/tap_se/default_rip/se_live_flick.mp3');
-//var audio2_2 = new Audio('https://storage.sekai.best/sekai-assets/live/tap_se/default_rip/se_live_flick.mp3');
-//var audio3_2 = new Audio('https://storage.sekai.best/sekai-assets/live/tap_se/default_rip/se_live_flick.mp3');
-//var audio4_2 = new Audio('https://storage.sekai.best/sekai-assets/live/tap_se/default_rip/se_live_flick.mp3');
-//var audio5_2 = new Audio('https://storage.sekai.best/sekai-assets/live/tap_se/default_rip/se_live_flick.mp3');
-//var audio6_2 = new Audio('https://storage.sekai.best/sekai-assets/live/tap_se/default_rip/se_live_flick.mp3');
-//var audio7_2 = new Audio('https://storage.sekai.best/sekai-assets/live/tap_se/default_rip/se_live_flick.mp3');
-//var audio8_2 = new Audio('https://storage.sekai.best/sekai-assets/live/tap_se/default_rip/se_live_flick.mp3');
-//var audio9_2 = new Audio('https://storage.sekai.best/sekai-assets/live/tap_se/default_rip/se_live_flick.mp3');
+function playsound(a) {
+	if (a === "tap") {
+		var audio = new Audio('./sound/tap.mp3');
+		audio.play();
+	};
+}
